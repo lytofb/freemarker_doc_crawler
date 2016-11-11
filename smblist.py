75a13d47c25322d766550bdf3e3db0cade49f452
root <303797380@qq.com>
2016-11-11 16:52:30 +0800

    重新封装自定义的smbwalk

--- smblist.py
+++ smblist.py
@@ -1,28 +1,40 @@
 __author__ = 'root'
 from smb.SMBConnection import SMBConnection
 import os;
-def smbwalk(top,conn):
-    yield from _smbwalk(top,conn);
+def smbwalk(self,top):
+    yield from self._smbwalk(top);
 
-def _smbwalk(top,conn):
-    shareds = conn.listpath_ex(top)
+def _smbwalk(self,top):
+    shareds = self.listpath_ex(top)
+    dirs = [];
+    files = [];
     for share in shareds:
         filename = share.filename;
         isdirectory = share.isDirectory;
-        yield top,filename,isdirectory;
         if filename=="." or filename=="..":
             continue
         if isdirectory:
-            thistop = os.path.join(top,filename)
-            yield from _smbwalk(thistop,conn)
+            dirs.append(filename)
+        else :
+            files.append(filename)
+    yield top,dirs,files
+    for thisdir in dirs:
+        thistop = os.path.join(top,thisdir)
+        yield from self._smbwalk(thistop)
 
 def listpath_ex(self, path):
     return self.listPath(self.remote_sv,path)
 
-MySMBConnection = type('MySMBConnection',(SMBConnection,),{"listpath_ex":listpath_ex,"remote_sv":'knose'})
-conn = MySMBConnection('root', 'p@ssw0rd', 'lytofb', 'root', use_ntlm_v2 = True)
-conn.connect('172.28.217.66', 139)
-smbresult = smbwalk("/",conn)
+def mysmbcon_factory(host,remote_server,username,password,my_name='default_name',port=139,remote_name='root',use_ntlm_v2 = True):
+    MySMBConnection = type('MySMBConnection',(SMBConnection,),{"listpath_ex":listpath_ex,"remote_sv":remote_server,"smbwalk":smbwalk,"_smbwalk":_smbwalk})
+    conn = MySMBConnection(username, password, my_name, remote_name, use_ntlm_v2 = use_ntlm_v2)
+    conn.connect(host, port)
+    return conn;
+# MySMBConnection = type('MySMBConnection',(SMBConnection,),{"listpath_ex":listpath_ex,"remote_sv":'knose'})
+# conn = MySMBConnection('root', 'p@ssw0rd', 'lytofb', 'root', use_ntlm_v2 = True)
+# conn.connect('172.28.217.66', 139)
+conn = mysmbcon_factory('172.28.217.66','knose','root','p@ssw0rd')
+smbresult = conn.smbwalk("/maven")
 for r in smbresult:
     print(r)
 # shares = conn.listShares()
